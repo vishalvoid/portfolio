ab81189bcdec26a564b4c641a35174b4031bb2cc
sagar88826 <sagarmandal88826@gmail.com>
2023-01-20 19:52:20 +0530

    rebase

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "./components/navbar/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <nav className="myname"></nav>
+    </>
+  );
+}
+
+export default App;
